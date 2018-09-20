12a4eac4168db6c88364f3dbcc6b40c3f7e5721e
unknown <olivier.kopp@heig-vd.ch>
2018-09-20 22:58:30 +0200

    implementation widget

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var followed = 0;
+function toggleFollow() {
+	if (followed == 0){
+		followed = 1;
+		document.getElementById("button-follow").style["display"] = "none";
+		document.getElementById("button-following").style["display"] = "inline-block";
+	}
+	else{
+		followed = 0;
+		document.getElementById("button-follow").style["display"] = "inline-block";
+		document.getElementById("button-following").style["display"] = "none";
+	}
+ }
