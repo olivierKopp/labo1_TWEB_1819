ec06ac655fb3c3ffab81e7582928c29ce461f4e3
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-09-25 09:41:10 +0200

    modification script de demarrage

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var path = require('path');
 
-const PORT=8080; 
+app.use(express.static(path.join(__dirname)));
+app.use("/css", express.static(__dirname));
+app.use("/img", express.static(__dirname + '/img'));
+app.use("/js", express.static(__dirname + '/js'));
 
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
+// viewed at based directory http://localhost:8080/
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'index.html'));
 });
+
+app.listen(process.env.PORT || 8080);
