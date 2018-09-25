fdbc5c4b98f074fc46acee5ee16744e57c934cc1
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-09-25 08:56:43 +0200

    version finale

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # labo1_TWEB_1819
+html page at https://labo1tweb1819-wsjsdfbwwk.now.sh
--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ var followed = 0;
 function toggleFollow() {
 	if (followed == 0){
 		followed = 1;
-		document.getElementById("button-follow").style["display"] = "none";
-		document.getElementById("button-following").style["display"] = "inline-block";
+		document.getElementById('button-follow').style['display'] = 'none';
+		document.getElementById('button-following').style['display'] = 'inline-block';
 	}
 	else{
 		followed = 0;
-		document.getElementById("button-follow").style["display"] = "inline-block";
-		document.getElementById("button-following").style["display"] = "none";
+		document.getElementById('button-follow').style['display'] = 'inline-block';
+		document.getElementById('button-following').style['display'] = 'none';
 	}
  }
